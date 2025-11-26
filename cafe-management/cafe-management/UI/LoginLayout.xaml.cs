291fd074de9b6d03cd73998b059a02d776f9664c
nvh1811 <153885271+nvh1811@users.noreply.github.com>
2025-11-26 18:31:33 +0700

    add folder ui

---
+++ cafe-management/cafe-management/UI/LoginLayout.xaml.cs
@@ -0,0 +1,46 @@
+﻿using cafe_management.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cafe_management.UI
+{
+    /// <summary>
+    /// Interaction logic for LoginLayout.xaml
+    /// </summary>
+    public partial class LoginLayout : UserControl
+    {
+        public LoginLayout()
+        {
+            InitializeComponent();
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Password;
+            if(AccountController.Instance.login(user, pass))
+            {
+                // Đăng nhập thành công
+                // Chuyển đến giao diện chính của ứng dụng
+                MainWindow main = (MainWindow)Window.GetWindow(this);
+                main.LoadHomeLayout();
+            }
+            else
+            {
+                // Đăng nhập thất bại
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng.", "Lỗi đăng nhập", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
